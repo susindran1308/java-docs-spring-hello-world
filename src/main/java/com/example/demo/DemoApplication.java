4e33da2b176e49f1a3850f36cce88ae29404ce08
susindran1308 <40034152+susindran1308@users.noreply.github.com>
2023-06-14 15:32:25 +0530

    changing status value with 200 as more probaility

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Random;
+import java.util.*;
 
 @SpringBootApplication
 @RestController
@@ -31,8 +31,26 @@ public class DemoApplication {
     }
 
     private HttpStatus getRandomStatusCode() {
-        Random random = new Random();
-        int statusCode = random.nextInt(201) + 300; // Generates a random number between 300 and 500 (inclusive)
+
+		List<Integer> httpErrorCodes = Arrays.asList(
+                300, 301, 302, 303, 304, 305, 307, 400, 401, 402,
+                403, 404, 405, 406, 407, 408, 409, 410, 411, 412,
+                413, 414, 415, 416, 417, 418, 421, 422, 423, 424,
+                426, 428, 429, 431, 451, 500, 501, 502, 503, 504,
+                505, 506, 507, 508, 510, 511,
+				200, 200, 200, 200, 200, 200, 200, 200,200, 200,200, 200, 200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,
+				200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,
+				200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,
+				200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,
+				200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,
+				200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200,200, 200
+        );
+
+        int randomIndex = getRandomIndex(httpErrorCodes.size());
+        int randomHttpErrorCode = httpErrorCodes.get(randomIndex);
+
+        //Random random = new Random();
+        //int statusCode = random.nextInt(100) + 300; // Generates a random number between 300 and 500 (inclusive)
         return HttpStatus.valueOf(statusCode);
     }
 }
